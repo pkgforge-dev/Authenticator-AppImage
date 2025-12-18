01d32bb4979e1a65666cad94284595599d9c4533
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-18 12:54:04 +0100

    Add `GTK_CLASS_FIX` for StartupWMClass fix on Wayland

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,7 +12,8 @@ export ICON=/usr/share/icons/hicolor/scalable/apps/com.belmoussaoui.Authenticato
 export DESKTOP=/usr/share/applications/com.belmoussaoui.Authenticator.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1 # For camera portal
-export STARTUPWMCLASS=authenticator # For Wayland, this is 'com.belmoussaoui.Authenticator', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
+export STARTUPWMCLASS=authenticator
+export GTK_CLASS_FIX=1
 
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
 quick-sharun /usr/bin/authenticator
